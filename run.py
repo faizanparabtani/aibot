e3ad205c3788ddcc0bdf2594b6ee775f416b75b2
Faizan Parabtani <faizan11911@gmail.com>
2021-10-18 14:26:04 +0530

    Added querying

--- run.py
+++ run.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import os
+import openpyxl
+from openpyxl.utils import get_column_letter
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+
+from opencage.geocoder import OpenCageGeocode
+
+
 
 filenumber=int(os.listdir('saved_conversations')[-1])
 filenumber=filenumber+1
@@ -21,10 +27,41 @@ english_bot = ChatBot('Bot',
    {
        'import_path': 'chatterbot.logic.BestMatch'
    },
-   
+
 ],
 trainer='chatterbot.trainers.ListTrainer')
 english_bot.set_trainer(ListTrainer)
+
+def get_doctor_from_city(diagnosis, city):
+    print(diagnosis, city)
+    filtered_doctors = []
+    for i in diagnosis:
+        if city in i[1]:
+            filtered_doctors.append(i)
+    return filtered_doctors
+
+
+def get_doctor_from_diagnosis(diagnosis):
+    wb = openpyxl.load_workbook('doctor_data.xlsx')
+    ws = wb.active
+    doctor_list = []
+    for row in range(2, ws.max_row+1):
+        col = "M"
+        if diagnosis == ws[col+str(row)].value:
+            doctor_name = ws["A"+str(row)].value + " " + ws["B"+str(row)].value
+            doctor_address = str(ws["D"+str(row)].value) + str(ws["F"+str(row)].value) + str(ws["G"+str(row)].value) + str(ws["H"+str(row)].value)
+            doctor_specialty = str(ws["L"+str(row)].value)
+            doctor_list.append((doctor_name, doctor_address, doctor_specialty))
+    return doctor_list
+
+def get_location():
+    # Location
+    key = '3265fdccd6a348a8b506da1cc60cd23e'
+    geocoder = OpenCageGeocode(key)
+
+    # Send location from geolocation to method below
+    results = geocoder.reverse_geocode(19.0485642, 72.8240584)
+    return results
 
 @app.route("/")
 def home():
@@ -34,6 +71,34 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
     response = str(english_bot.get_response(userText))
+    # Diagnosis
+    user_response_list = userText.split(' ')
+    bot_response = response.split(' ')
+    diagnosis = ''
+    city = ''
+    if 'live' in user_response_list or 'stay' in user_response_list or 'reside' in user_response_list:
+        city = user_response_list[-1]
+
+    if 'fracture' in bot_response:
+        diagnosis = 'Fracture'
+    elif 'headache' in bot_response or 'headaches' in bot_response:
+        diagnosis = 'Headache'
+    elif 'cold.' in bot_response or 'cough.' in bot_response:
+        diagnosis = 'Cold Cough'
+    elif 'fever' in bot_response:
+        diagnosis = 'Fever'
+    elif 'anxiety.' in bot_response:
+        diagnosis = 'Anxiety'
+    elif 'itch' in bot_response or 'swallowing food' in bot_response or 'ear-wax' in bot_response:
+        diagnosis = 'ENT'
+
+
+    if diagnosis != "":
+        get_diagnosis = get_doctor_from_diagnosis(diagnosis)
+        if city != "":
+            get_doctors = get_doctor_from_city(get_diagnosis, city)
+            print(get_doctors)
+
 
     appendfile=os.listdir('saved_conversations')[-1]
     appendfile= open('saved_conversations/'+str(filenumber),"a")
